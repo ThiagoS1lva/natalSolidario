452b8f763502c5ed65bfc3f1a165e47af557204e
Thiago Oliveira <113373282+ThiagoS1lva@users.noreply.github.com>
2024-11-13 22:54:44 -0300

    v2

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@
       </video>
     </div>
     <div class="image-row">
+      <h2 style="text-align: center;"> Só clicar na criança que você gostaria de apadrinhar! </h2>
       <div class="image-column" v-for="(item, index) in images" :key="index">
         <a :href="`https://api.whatsapp.com/send?phone=5522999330628&text=Eu%20quero%20apadrinhar%3A%20${item.alt}`"><img :src="item.src" :alt="item.alt" /></a>
         <p>{{ item.text }}</p>
@@ -23,10 +24,19 @@ export default {
   data() {
     return {
       images: [
-        { src: "/Yasmin.jpg", alt: "Yasmin", text: "Yasmin - Um Brinquedo para bebê" },
-        { src: "/Levi.jpg", alt: "Levi", text: "Levi - Meu sonho é ganhar uma bicicleta ou roupas" },
-        { src: "/AnnaLiz.png", alt: "Anna Liz", text: "Yasmin - Um Brinquedo para bebê" },
-        { src: "/Levi.jpg", alt: "Levi", text: "Levi - Meu sonho é ganhar uma bicicleta ou roupas" }
+        { src: "/Yasmin.jpg", alt: "Yasmin", text: "Yasmin - Um Brinquedo para bebê!" },
+        { src: "/Levi.jpg", alt: "Levi", text: "Levi - Meu sonho é ganhar uma bicicleta ou roupas!" },
+        { src: "/AnnaLiz.png", alt: "Anna Liz", text: "Anna Liz - Brinquedo ou roupa de 3 anos veste 2 anos(18-24)!" },
+        { src: "/Fernanda.jpg", alt: "Fernanda", text: "Fernanda - Uma bermuda tamanho 42 infantil ou kit maquiagem!" },
+        { src: "/1.jpg", alt: "1", text: "Um cavalo de brinquedo!" },
+        { src: "/JoaoLucas.png", alt: "João Lucas", text: "João Lucas - Tenho 10 anos e gostaria de uma roupa!" },
+        { src: "/Victor Hugo.jpg", alt: "Victor Hugo", text: "Victor Hugo - Uma moto de brinquedo da grande!" },
+        { src: "/Pietro.jpg", alt: "Pietro", text: "Pietro - Um boneco do homem aranha!" },
+        { src: "/Julia.jpg", alt: "Julia", text: "Julia - Roupa 38, short ou blusa!" },
+        { src: "/Maria Tereza.jpg", alt: "Maria Tereza", text: "Maria Tereza - Eu gostaria de ganhar um conjunto de cordão e pulseira!" },
+        { src: "/c1.jpg", alt: "c1", text: "c1 - Uma roupa da Nike!" },
+        { src: "/c2.jpg", alt: "c2", text: "c2 - Uma bola!" },
+        { src: "/c3.jpg", alt: "c3", text: "c3 - Um relógio e brincos!" }
       ]
     };
   }
@@ -98,7 +108,7 @@ video {
   }
   
   .video-container video {
-    width: 100%; /* Ajusta o vídeo para a largura completa em dispositivos móveis */
+    width: 90%; /* Ajusta o vídeo para a largura completa em dispositivos móveis */
     height: auto;
   }
 }
