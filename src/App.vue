3dafc55eb5a2c514e600639361e3430abb325568
Thiago Oliveira <113373282+ThiagoS1lva@users.noreply.github.com>
2024-11-13 22:36:52 -0300

    up2

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@
     </div>
     <div class="image-row">
       <div class="image-column" v-for="(item, index) in images" :key="index">
-        <img :src="item.src" :alt="item.alt" />
+        <a :href="`https://api.whatsapp.com/send?phone=5522999330628&text=Eu%20quero%20apadrinhar%3A%20${item.alt}`"><img :src="item.src" :alt="item.alt" /></a>
         <p>{{ item.text }}</p>
       </div>
     </div>
@@ -23,9 +23,10 @@ export default {
   data() {
     return {
       images: [
-        { src: "./assets/Yasmin.jpg", alt: "Yasmin", text: "Yasmin - Um Brinquedo para bebê" },
-        { src: "./assets/Yasmin.jpg", alt: "Yasmin", text: "Yasmin - Um Brinquedo para bebê" },
-        { src: "./assets/Yasmin.jpg", alt: "Yasmin", text: "Yasmin - Um Brinquedo para bebê" }
+        { src: "/Yasmin.jpg", alt: "Yasmin", text: "Yasmin - Um Brinquedo para bebê" },
+        { src: "/Levi.jpg", alt: "Levi", text: "Levi - Meu sonho é ganhar uma bicicleta ou roupas" },
+        { src: "/AnnaLiz.png", alt: "Anna Liz", text: "Yasmin - Um Brinquedo para bebê" },
+        { src: "/Levi.jpg", alt: "Levi", text: "Levi - Meu sonho é ganhar uma bicicleta ou roupas" }
       ]
     };
   }
@@ -51,33 +52,35 @@ video {
 .navbar {
   background-color: #ff4d4d; /* cor de fundo */
   padding: 1rem;
+  width: 100vw;
   text-align: center;
 }
 
 .navbar h1 {
   color: #fff;
-  font-size: 1.5rem;
+  font-size: 2rem;
   margin: 0;
 }
 
 /* Estilo para a fileira de imagens */
 .image-row {
-  display: flex;
-  justify-content: center;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr); /* 3 colunas em telas maiores */
   gap: 1rem;
+  justify-items: center;
 }
 
 .image-column {
   display: flex;
   flex-direction: column;
   align-items: center;
-  width: 150px; /* Define a largura fixa das colunas */
+  width: 200px; /* Define a largura fixa das colunas */
 }
 
 .image-column img {
-  width: 100%;
+  width: 200px;
   height: auto;
-  max-height: 150px; /* Define uma altura máxima para as imagens */
+  max-height: 250px; /* Define uma altura máxima para as imagens */
   object-fit: cover; /* Ajusta a imagem para cobrir o espaço, mantendo o centro */
   border-radius: 8px;
 }
@@ -85,6 +88,18 @@ video {
 .image-column p {
   margin-top: 0.5rem;
   text-align: center;
-  font-size: 1rem;
+  font-size: 1.2rem;
+}
+
+/* Responsividade para telas menores */
+@media (max-width: 768px) {
+  .image-row {
+    grid-template-columns: 1fr; /* Altera para uma coluna */
+  }
+  
+  .video-container video {
+    width: 100%; /* Ajusta o vídeo para a largura completa em dispositivos móveis */
+    height: auto;
+  }
 }
 </style>
