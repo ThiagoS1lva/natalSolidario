714966876c5b3591f6cb33d8c5aa09209c5a094d
Thiago Oliveira <113373282+ThiagoS1lva@users.noreply.github.com>
2024-11-16 19:08:41 -0300

    final

--- src/App.vue
+++ src/App.vue
@@ -9,12 +9,17 @@
         Seu navegador não suporta o elemento de vídeo.
       </video>
     </div>
-    <div class="image-row">
+    <div style="margin-bottom: 2em; width: 100%;">
       <h2 style="text-align: center;"> Só clicar na criança que você gostaria de apadrinhar! </h2>
+    </div>
+    <div class="image-row">
       <div class="image-column" v-for="(item, index) in images" :key="index">
         <a :href="`https://api.whatsapp.com/send?phone=5522999330628&text=Eu%20quero%20apadrinhar%3A%20${item.alt}`"><img :src="item.src" :alt="item.alt" /></a>
         <p>{{ item.text }}</p>
       </div>
+    </div>
+    <div class="footer">
+      <p>Feliz natal!</p>
     </div>
   </div>
 </template>
@@ -24,19 +29,32 @@ export default {
   data() {
     return {
       images: [
-        { src: "/Yasmin.jpg", alt: "Yasmin", text: "Yasmin - Um Brinquedo para bebê!" },
+      { src: "/Maria Alice.jpg", alt: "Maria Alice", text: "Maria Alice - Maquiagem, boneca reborn!" },
+      { src: "/Jhonatan.jpg", alt: "Jhonatan", text: "Jhonatan - Brinquedinho de fazenda (boi, cavalo, etc...)!" },
+      { src: "/Yasmin.jpg", alt: "Yasmin", text: "Yasmin - Um Brinquedo para bebê!" },
+      { src: "/Anna Vallentyna.jpg", alt: "Anna Vallentyna", text: "Anna Vallentyna - Uma Boneca!" },
+      { src: "/b1.jpg", alt: "b1", text: "Umas roupinhas ou um brinquedo!" },
+      { src: "/Mellynda.jpg", alt: "Mellynda", text: "Mellynda - Um brinquedo da galinha pintadinha!" },
+      { src: "/Ravi.jpg", alt: "Ravi", text: "Ravi - Brinquedos de animais de fazenda!" },
+      { src: "/Hevellyn.jpg", alt: "Hevellyn", text: "Hevellyn - Maquiagem!" },
+      { src: "/Sophya.jpg", alt: "Sophya", text: "Sophya - Boneca!" },
         { src: "/Levi.jpg", alt: "Levi", text: "Levi - Meu sonho é ganhar uma bicicleta ou roupas!" },
         { src: "/AnnaLiz.png", alt: "Anna Liz", text: "Anna Liz - Brinquedo ou roupa de 3 anos veste 2 anos(18-24)!" },
         { src: "/Fernanda.jpg", alt: "Fernanda", text: "Fernanda - Uma bermuda tamanho 42 infantil ou kit maquiagem!" },
-        { src: "/1.jpg", alt: "1", text: "Um cavalo de brinquedo!" },
+        { src: "/Gael.jpg", alt: "Gael", text: "Gael - Um cavalo de brinquedo!" },
         { src: "/JoaoLucas.png", alt: "João Lucas", text: "João Lucas - Tenho 10 anos e gostaria de uma roupa!" },
         { src: "/Victor Hugo.jpg", alt: "Victor Hugo", text: "Victor Hugo - Uma moto de brinquedo da grande!" },
         { src: "/Pietro.jpg", alt: "Pietro", text: "Pietro - Um boneco do homem aranha!" },
         { src: "/Julia.jpg", alt: "Julia", text: "Julia - Roupa 38, short ou blusa!" },
+        { src: "/Giovanna.jpg", alt: "Giovanna", text: "Giovanna - Papete com brilho 37/38 ou roupa 14 anos!" },
         { src: "/Maria Tereza.jpg", alt: "Maria Tereza", text: "Maria Tereza - Eu gostaria de ganhar um conjunto de cordão e pulseira!" },
-        { src: "/c1.jpg", alt: "c1", text: "c1 - Uma roupa da Nike!" },
-        { src: "/c2.jpg", alt: "c2", text: "c2 - Uma bola!" },
-        { src: "/c3.jpg", alt: "c3", text: "c3 - Um relógio e brincos!" }
+        { src: "/Raquelly.jpg", alt: "Raquelly", text: "Raquelly - Conjunto Lilo & Stitch!" },
+        { src: "/Rodolfo.jpg", alt: "Rodolfo", text: "Rodolfo - Uma roupa da Nike!" },
+        { src: "/Rafael.jpg", alt: "Rafael", text: "Rafael - Uma bola!" },
+        { src: "/Clarice.jpg", alt: "Clarice", text: "Clarice - Um relógio e brincos!" },
+        { src: "/Alana.jpg", alt: "Alana", text: "Alana - Boneca!" },
+        { src: "/Maria Eduarda.jpg", alt: "Maria Eduarda", text: "Maria Eduarda - Roupas tamanho 36 ou sandália tamanho 37!" },
+        { src: "/Lavynia.jpg", alt: "Lavynia", text: "Lavynia - Uma bicicleta!" }
       ]
     };
   }
@@ -44,6 +62,16 @@ export default {
 </script>
 
 <style scoped>
+.footer {
+  background-color: #ff4d4d;
+  color: #fff;
+  text-align: center;
+  padding: 1rem;
+}
+.footer p {
+  font-size: 1.5em;
+}
+
 /* Centraliza o contêiner e ajusta o tamanho do vídeo */
 .video-container {
   display: flex;
